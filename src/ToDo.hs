9b2e4228df9569e4e1461ac3c46fa4c6f7ba0b60
danny o brien <danny@spesh.com>
2015-09-07 13:43:57 -0700

    Refactor parser

--- src/ToDo.hs
+++ src/ToDo.hs
@@ -6,59 +6,57 @@ module ToDo (
     ,TimeDate
     ,Line
     ,parseLine
-    ,makeTag
         ) where
-            
-import qualified Data.Text as T
-import qualified Data.Char as C
-import Data.Attoparsec.Text
-import Control.Applicative
-import Control.Monad.State
-import Data.Text
 
-type Recurrence = String 
-type TimeDate = String
-type Line = Text
+import           Control.Applicative
+import           Control.Monad.State
+import           Data.Attoparsec.Text
+import qualified Data.Char            as C
+import           Data.Text
+import qualified Data.Text            as T
 
-data Tag =  Current 
-          | Urgent 
-          | Repeat { interval :: Recurrence }
-          | Ignore { until :: TimeDate }
-          | Context { cName :: T.Text, cArgs :: T.Text } 
-          | Project { pName :: T.Text } 
+type Recurrence = T.Text
+type TimeDate = T.Text
+type Line = T.Text
+
+data Tag =  Current
+          | Urgent
+          | Repeat { rInterval :: Recurrence }
+          | Ignore { iUntil :: TimeDate }
+          | Context { cName :: T.Text, cArgs :: T.Text }
+          | Project { pName :: T.Text }
           deriving (Show, Eq)
 
 data TodoEntry = TodoEntry {
                      originalText :: Text,
-                     tags :: [Tag],
-                     indent :: Int
+                     tags         :: [Tag],
+                     indent       :: Int
                      } deriving (Show, Eq)
-                     
-makeTag x = Context { cName = x, cArgs = "" }
 
-current = do
-    string "@CURRENT"
-    return [Current]
- 
-project = do 
-    string "#"
+parseProject = do
+    _ <- string "#"
     n <- takeWhile1 (inClass "A-Za-z_")
     return [Project {pName = n}]
 
-args = do
-    string "("
+parseArgs = do
+    _ <- string "("
     a <- takeWhile1 (/= ')' )
-    string ")"
+    _ <- string ")"
     return a
     <|> return ""
-   
-tag = do
-    string "@"
-    n <- takeWhile1 (inClass "A-Za-z_")
-    b <- args
-    return [Context {cName = n, cArgs = b}]
 
-comment = do
+parseTag = do
+    _ <- string "@"
+    n <- takeWhile1 (inClass "A-Za-z_")
+    b <- parseArgs
+    case n of
+        "CURRENT"-> return [Current]
+        "URGENT" -> return [Urgent]
+        "IGNORE" -> return [Ignore {iUntil=b}]
+        "REPEAT" -> return [Repeat {rInterval=b}]
+        _ -> return [Context {cName = n, cArgs = b}]
+
+parseComment = do
     n <- takeWhile1 (not . inClass "@#")
     return []
 
@@ -66,28 +64,28 @@ indentCount = do
     x <- takeTill (not . C.isSpace)
     return (T.length x)
 
-addTags :: [Tag] -> TodoEntry -> TodoEntry
-addTags a b = b { tags = a ++ tags b }
-
 addTag :: Tag -> TodoEntry -> TodoEntry
 addTag a b = b { tags = a : tags b }
 
 storeIndent :: Int -> TodoEntry -> TodoEntry
 storeIndent a b = b { indent = a }
 
-parseLine l = parseOnly (runStateT parseLine' TodoEntry { indent = 0, originalText = l, tags = []}) l
-
-parseLine' :: StateT TodoEntry Parser ()
-parseLine' = do
-    i <- lift indentCount
-    (modify (storeIndent i))
-    many parseTags
-    pure ()
+parseLine :: Text -> TodoEntry
+parseLine l = parseLineHelper $ parseOnly (runStateT parseLine' TodoEntry { indent = 0, originalText = l, tags = []}) l
+              where
+              parseLineHelper (Right (_,b)) = b
+              parseLineHelper (Left _) = TodoEntry { indent = 0, originalText = "Error", tags = []}
+              parseLine' :: StateT TodoEntry Parser ()
+              parseLine' = do
+                  i <- lift indentCount
+                  modify (storeIndent i)
+                  _ <- many parseTags
+                  pure ()
 
 parseTags :: StateT TodoEntry Parser ()
 parseTags = do
-    a <- (lift current  <|> lift project  <|> lift tag <|> lift comment)
+    a <- lift parseProject  <|> lift parseTag <|> lift parseComment
     case a of
-        [t] -> (modify (addTag t))
+        [t] -> modify (addTag t)
         _ -> pure ()
     pure ()
--- test/ToDoSpec.hs
+++ test/ToDoSpec.hs
@@ -3,22 +3,33 @@ module ToDoSpec (main, spec) where
 
 import Test.Hspec
 import ToDo
-import Data.Text
-import Data.String
+
+
+import Data.List (find)
 
 main :: IO ()
 
 main = hspec spec
 
 sampleTodoLine = "    go buy a snark gun"
+sampleTodoLineWithTags :: String
 sampleTodoLineWithTags = "    go buy a snark gun @SHOPPING #SAFARI" 
 
 spec :: Spec
 spec =  do
-    describe "Tag" $ do
+    describe "Tag" $ 
         it "can create a Tag from a string" $
-            (show (Project "Foo")) `shouldBe` "Project {pName = \"Foo\"}"
+            show (Project "Foo") `shouldBe` "Project {pName = \"Foo\"}"
 
     describe "TodoEntry" $ do
         it "can create a TodoEntry from a string and preserve its contents" $
-            (parseLine "this is a line #CURRENT @MORNING") `shouldBe` (parseLine "this is a line #CURRENT @MORNING") 
+            parseLine "this is a line #CURRENT @MORNING" `shouldBe` parseLine "this is a line #CURRENT @MORNING" 
+
+        it "can spot a Current tag" $
+            Data.List.find (== Current) (tags $ parseLine "this is a @CURRENT line") `shouldBe` Just Current
+            
+        it "can spot no Current tag" $
+            (Data.List.find (== Current) $ tags $ parseLine "this is a line") `shouldBe` Nothing
+            
+        it "can spot an Urgent tag" $
+            (Data.List.find (== Urgent) $ tags $ parseLine "this is a @URGENT line") `shouldBe` Just Urgent
